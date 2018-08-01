2196fa6207bfec74edb2699dba489a7ff4e3c9ac
shainachen <shaina.sierra@gmail.com>
2018-08-01 14:34:51 -0700

    add basic break function

---
+++ background.js
@@ -0,0 +1,38 @@
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'images/get_started16.png',
+      title:    'Break is Over',
+      message:  'Ughh... already??',
+      buttons: [
+        {title: 'Extend break'}
+      ],
+      priority: 0});
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+  });
+});
+
+
+/*chrome.runtime.onInstalled.addListener(function() {
+	chrome.storage.sync.set({color: '#3aa757'}, function(){
+		console.log("The color is green.");
+	});
+
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+	chrome.declarativeContent.onPageChanged.addRules([{
+		conditions: [new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: {hostEquals: 'developer.chrome.com'},
+		})
+		],
+		actions: [new chrome.declarativeContent.ShowPageAction()]
+	}]);
+});
+});
+*/
